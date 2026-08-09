882d0698c5fdc35d1e1a83260da35150ebf4269f
Gurukiran <gurukiran@Gurukirans-MacBook-Air.local>
2026-08-09 17:00:19 +0530

    Fix missing auth token interceptor in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
 });
 
-export default API;
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
